79118785c3db7b80974593d54f93eb3ee4ed0f80
Chandra Holt <chandra_holt@hotmail.com>
2022-01-26 14:08:49 -0800

    Set up server.js file. Added required files, installed npm requisites

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+app.listen(PORT, () =>
+console.log(`Example app listening at http://localhost:${PORT}`)
+);
+
+// app.get('/', (req, res) => {
+//     res.send('Navigate to /send or /routes');
+// });
+
+// app.get('/send', (req, res) =>
+//     res.sendFile(path.join(__dirname, 'public/sendFile.html'))
+// })
