e3599f64d89c51b53290fb689aaebdf2f8afcb62
Chandra Holt <chandra_holt@hotmail.com>
2022-01-26 18:15:27 -0800

    Developer folder updated

--- Develop/server.js
+++ Develop/server.js
@@ -14,6 +14,17 @@ app.get('/', (req, res) => {
     res.send('Navigate to /send or /routes');
 });
 
+app.get('/api/notes', (req, res) => {
+    //send the file 'notes.html' 
+    res.json(/*send note data*/);
+});
+
+app.post('/api/notes', (res, req) => {
+    // Create (persist) data
+
+    //Push
+});
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 });
